de0dd5e57024fd2ef7df020bd1cf8d13d65e26d6
Manodiestra <52931506+Manodiestra@users.noreply.github.com>
2022-03-15 21:12:12 -0600

    Fixed typos and made it so all sections expand on aboutme and cv pages

--- src/js/aboutMePageScript.js
+++ src/js/aboutMePageScript.js
@@ -1,6 +1,15 @@
 
 // Open the where I am component for the user
 let whereIAmToggle = document.getElementById("amContentToggle");
+let whereIWasToggle = document.getElementById("wasContentToggle");
+let whereIGoingToggle = document.getElementById("goingContentToggle");
+
 setTimeout(function() {
   whereIAmToggle.click();
 }, 500);
+setTimeout(function() {
+  whereIWasToggle.click();
+}, 300);
+setTimeout(function() {
+  whereIGoingToggle.click();
+}, 100);
--- src/js/cvPageScript.js
+++ src/js/cvPageScript.js
@@ -1,5 +1,14 @@
 // Open the where I am component for the user
-let whereIAmToggle = document.getElementById("educationContentToggle");
+let educationToggle = document.getElementById("educationContentToggle");
+let experienceToggle = document.getElementById("experienceContentToggle");
+let skillsToggle = document.getElementById("skillsContentToggle");
+
 setTimeout(function() {
-  whereIAmToggle.click();
-}, 500);
+  educationToggle.click();
+}, 550);
+setTimeout(function() {
+  experienceToggle.click();
+}, 330);
+setTimeout(function() {
+  skillsToggle.click();
+}, 100);
